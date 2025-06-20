acf48021c0d74960aba03d5a093fa38361cc4a74
Naven Christian Laya <navenchristian.laya@gmail.com>
2025-06-20 06:30:25 -0700

    View to forcast user's spending using prophet

--- finance_tracker/transactions/views.py
+++ finance_tracker/transactions/views.py
@@ -6,8 +6,13 @@ from .forms import UploadCSVForm
 from .models import Transaction
 from .utils import categorize
 
+# For Forcast View
+import pandas as pd
+from prophet import Prophet
+
 
 @login_required
+# Transaction Upload View
 def upload_transactions(request):
     if request.method == 'POST':
         form = UploadCSVForm(request.POST, request.FILES)
@@ -15,6 +20,7 @@ def upload_transactions(request):
             file = TextIOWrapper(request.FILES['csv_file'].file, encoding='utf-8')
             reader = csv.DictReader(file)
 
+            # Clear existing transactions for the user before uploading new ones
             for row in reader:
                 Transaction.objects.create(
                     user=request.user,
@@ -30,7 +36,41 @@ def upload_transactions(request):
     return render(request, 'transactions/upload.html', {'form': form})
 
 @login_required
+# Transaction List View
 def transaction_list(request):
     transactions = Transaction.objects.filter(user=request.user).order_by('-date')
     return render(request, 'transactions/list.html', {'transactions': transactions})
 
+@login_required
+def forcast_view(request):
+    # Get user's transactions
+    transactions = Transaction.objects.filter(user=request.user).order_by('date')
+
+    df = pd.DataFrame.from_records(
+        transactions.values('date', 'amount'),
+
+    )
+
+    df = df.groupby('date').sum().reset_index()
+
+    if df.empty:
+        return (request, 'transactions/forcast.html', {'message': 'No data to forcast'})
+    
+    df['ds'] = pd.to_datetime(df['date'])
+    df['y'] = df['amount']
+
+    # Create and train prophet model
+    model = Prophet()
+    model.fit(df[['ds', 'y']])
+
+    # Create future days
+    future = model.make_future_dataFrame(period=30)
+    forecast = model.predict(future)
+
+    # Convert to JSON for fontend
+    forcast_json = forecast [['ds', 'yhat', 'yhat_lower', 'yhat_upper']].to_json(orient='records', date_format='iso')
+
+    return render(request, 'transactions/forcast.html', {
+        'forcast_json': forcast_json,
+        'forcast_table': forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(10).to_html(classes='table table-striped'),
+    })
