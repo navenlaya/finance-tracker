86c75a5b2563910acedced745f809b1c326cc116
Naven Christian Laya <navenchristian.laya@gmail.com>
2025-06-24 02:17:38 -0700

    Connected Url for Insight View

--- finance_tracker/transactions/urls.py
+++ finance_tracker/transactions/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('upload/', views.upload_transactions, name='upload-transactions'),
     path('', views.transaction_list, name='transactions-list'),
     path('forecast/', views.forecast_view, name='forcast'), 
+    path('insights/', views.insights_view, name='insights'),
 ]
