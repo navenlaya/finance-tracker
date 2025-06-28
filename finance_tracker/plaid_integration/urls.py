e9a5d940435f51badc11a63193534bc14dc3b16d
Naven Christian Laya <navenchristian.laya@gmail.com>
2025-06-28 06:35:23 -0700

    connected plaid integration app url

--- finance_tracker/plaid_integration/urls.py
+++ finance_tracker/plaid_integration/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-link-token/', views.create_link_token, name='create-link-token'),
-    path('exchange-token/', views.exchange_token, name='exchange-token'),
+    path("create-link-token/", views.create_link_token, name="create-link-token"),
+    path("exchange-token/", views.exchange_token, name="exchange-token"),
+    path("connect/", views.plaid_link_page, name="plaid-link-page"),  # ⬅️ New
 ]
