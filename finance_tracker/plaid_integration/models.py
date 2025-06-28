f7d054231843fba4f74894159859cef3af1efb6a
Naven Christian Laya <navenchristian.laya@gmail.com>
2025-06-28 06:35:54 -0700

    model to store each user's item info

--- finance_tracker/plaid_integration/models.py
+++ finance_tracker/plaid_integration/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Stores each user's Plaid item information
+class PlaidItem(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=200)
+    item_id = models.CharField(max_length=200)
