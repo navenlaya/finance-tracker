1373f5e281c3fb94a16adb9e928190494e4b543b
Naven Christian Laya <navenchristian.laya@gmail.com>
2025-07-07 13:15:27 -0700

    User Model, Auth Views and Routes

---
+++ backend/app/crud/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def create_user(db: Session, user: UserCreate):
+    hashed_pw = pwd_context.hash(user.password)
+    db_user = User(email=user.email, hashed_password=hashed_pw)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user or not pwd_context.verify(password, user.hashed_password):
+        return None
+    return user
