e7b1500587d38c42e3e30c4d14dc08b772447509
Naven Christian Laya <navenchristian.laya@gmail.com>
2025-07-19 23:10:12 -0700

    updated main app setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from app.api import api_router
 from app.db.session import Base, engine
 from app.models import user, plaid  
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # or ["*"] for all origins (less secure)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router)
